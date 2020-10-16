9b11dd5b05774ebc51997a404516436bab320ca2
buitron <ivanandresbuve@unicauca.edu.co>
2020-10-16 13:12:20 -0500

    Ultimo commit codigo funcionando

--- Servidor.py
+++ Servidor.py
@@ -14,48 +14,46 @@ while True:
                      b'\n 1. Dolar \n 2. Euro \n 3. Peso Mexicano \n 4. Salir')
 
     p = 0
+    op = 4
     while p <= 0 or p >= 5:
         try:
-            recibido = conexion.recv(1024).decode()
-            op = int(recibido)
+            op = int(conexion.recv(1024).decode())
             p = op
-            print("Del cliente recibi: ", recibido)
-            if op < 1 or op > 4 or not recibido:
+            print("Del cliente recibi: ", op)
+            if op < 1 or op > 4 or not op:
                 conexion.sendall(b'La opcion no es valida! *Por favor ingrese una opcion de 1 a 4*')
         except ValueError:
             conexion.sendall(b'No ingreso un numero! *Por favor ingrese una opcion de 1 a 4*')
 
+    valor = 0
     if op != 4:
         conexion.sendall(b'Ingresa el valor en pesos Colombianos a convertir: ')
-        v = 0
-        while v <= 0:
+        while valor <= 0:
             try:
-                valor = conexion.recv(1024).decode()
-                valorconv = float(valor)
-                v = valorconv
+                valor = float(conexion.recv(1024).decode())
                 print("Del cliente recibi: ", valor)
-                if valorconv <= 0 or not valor:
+                if valor <= 0 or not valor:
                     conexion.sendall(b'El numero ingresado es negativo o cero! *Ingrese un valor mayor a 0*')
             except ValueError:
                 conexion.sendall(b'No ha ingresado un numero! *Por favor ingrese un valor mayor a 0*')
 
     if op == 1:
-        ResultadoDolar = valorconv / 3829.70
-        conexion.sendall((str(valorconv) + ' pesos Colombianos son ' + str(ResultadoDolar) + ' Dolares'
+        ResultadoDolar = valor / 3829.70
+        conexion.sendall((str(valor) + ' pesos Colombianos son ' + str(ResultadoDolar) + ' Dolares'
                           + '\n*Presiones Enter para continuar*').encode())
-        print(valorconv, 'pesos Colombianos es igual a', ResultadoDolar, 'Dolares')
+        print(valor, 'pesos Colombianos es igual a', ResultadoDolar, 'Dolares')
 
     elif op == 2:
-        ResultadoEuro = valorconv / 4456.05
-        conexion.sendall((str(valorconv) + ' pesos Colombianos es igual a ' + str(ResultadoEuro) + ' Euros'
+        ResultadoEuro = valor / 4456.05
+        conexion.sendall((str(valor) + ' pesos Colombianos es igual a ' + str(ResultadoEuro) + ' Euros'
                           + '\n*Presiones Enter para continuar*').encode())
-        print(valorconv, 'pesos Colombianos es igual a', ResultadoEuro, 'Euros')
+        print(valor, 'pesos Colombianos es igual a', ResultadoEuro, 'Euros')
 
     elif op == 3:
-        ResultadoMex = valorconv / 174.06
-        conexion.sendall((str(valorconv) + ' pesos Colombianos son ' + str(ResultadoMex) + ' pesos Mexicanos'
+        ResultadoMex = valor / 174.06
+        conexion.sendall((str(valor) + ' pesos Colombianos son ' + str(ResultadoMex) + ' pesos Mexicanos'
                           + '\n*Presiones Enter para continuar*').encode())
-        print(valorconv, 'pesos Colombianos es igual a', ResultadoMex, 'pesos Mexicanos')
+        print(valor, 'pesos Colombianos es igual a', ResultadoMex, 'pesos Mexicanos')
 
     elif op == 4:
         conexion.sendall(b'Gracias por usar el conversor de monedas \n *Presiona Enter para salir*')
